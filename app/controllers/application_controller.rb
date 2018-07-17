49f4b2fdccf2c586d3261c24754bfe01ca69d874
rebeccaeli20 <rebeccaeli20@gmail.com>
2018-07-17 21:22:25 +0000

    stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,7 @@
-require './config/environment'
-require './app/models/sample_model'
-
-class ApplicationController < Sinatra::Base
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
-
-  get '/' do
-    return erb :index
+past "/" do 
+    @first_response = params[ :response1]
+    @second_response = params[ :response2]
+    @result = kwiz_return[@first_response, @second_response]
+    return erb :results
   end
 end
--- app/models/sample_model.rb
+++ app/models/sample_model.rb
@@ -1,3 +1,13 @@
-class SampleModel
-  attr_accessor :name, :age
+def kwiz_return(response1, response2)
+  if response1 == "array" && response2 == "pancakes"
+    "WEIRD! YOU are an array who likes pancakes!"
+  elsif response1 == "array" && response2 == "waffles"
+    "OOOOOOOOOOOOOOO SPIIIIICEEEEY"
+  elsif response1 == "hash" && response2 == "pancakes"
+    "you are insufferable"
+  elsif response1 == "hash" && response2 == "waffles"
+    "hash and waffles? more like hashBROWNS and waffles"
+  else
+    "welp! you done did it now, didcha?"
+  end 
 end
